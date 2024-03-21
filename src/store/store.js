71f4fa9c3637c879ac53e8a72be713f1194654a0
Nebajooo <nebajoo2015@gmail.com>
2024-03-21 13:33:25 -0700

    update project code

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./shopping-cart/cartSlice";
 import cartUiSlice from "./shopping-cart/cartUiSlice";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
     cartUi: cartUiSlice.reducer,
-    middleware: [thunk, logger],
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
